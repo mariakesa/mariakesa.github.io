89d1be8438466cd7aba82555708b1c373b5887f2
mariakesa <maria.kesa@gmail.com>
2020-12-30 01:53:42 +0100

    images

---
+++ index.js
@@ -0,0 +1,37 @@
+const canvas=d3.select(".canvas")
+
+var w = 100,
+    h = 100,
+    z = 20,
+    x = 100 / z,
+    y = 100 / z;
+
+var svg = canvas.append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+var receptive_field = canvas.append("img")
+  .attr('width', 200)
+  .attr('height', 100)
+  .attr("href", "https://github.com/mariakesa/mariakesa.github.io/blob/main/img/ensemble_rf/ensemble_rf_0.png")
+
+svg.selectAll("rect")
+    .data(d3.range(x * y))
+    .enter().append("rect")
+    .attr("transform", translate)
+    .attr("width", z)
+    .attr("height", z)
+    .style("fill", function(d) { return d3.hsl(d % x / x * 360, 1, Math.floor(d / x) / y); })
+    .on("click", mouseover);
+
+
+function translate(d) {
+      return "translate(" + (d % x) * z + "," + Math.floor(d / x) * z + ")";
+}
+
+function mouseover(d) {
+      d3.select(this)
+      console.log(d)
+      image=d3.select("img")
+        .attr("href", "https://github.com/mariakesa/mariakesa.github.io/blob/main/img/ensemble_rf/ensemble_rf_"+d+".png")
+    }
