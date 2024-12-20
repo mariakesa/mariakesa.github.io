b3c78c7c3907c2bf7550b5ce6ab0e5e80e215be7
Maria Kesa <maria.kesa@gmail.com>
2024-12-20 15:05:55 +0200

    the black square

--- algorithm_tutorials/linear_algebra_basics/vector.js
+++ algorithm_tutorials/linear_algebra_basics/vector.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const height = 50;
 
     // Initialize the scalar value (integer by default)
-    let scalarValue = getRandomInteger();
 
     // Create an SVG element
     const svg = container.append("svg")
@@ -19,4 +18,12 @@ document.addEventListener("DOMContentLoaded", function () {
         .attr("width", width)
         .attr("height", height)
         .attr("class", "vector-square");
+
+    scalarValue= "hello";
+    const text = svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height / 2)
+        .attr("class", "vector-value")
+        .text(scalarValue);
+
 });
