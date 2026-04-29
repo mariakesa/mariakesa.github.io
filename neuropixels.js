603d823abbc145097f3f698e6cff095c4440f94e
Maria Kesa <maria.kesa@gmail.com>
2026-04-29 20:35:49 +0300

    fix centering issue with index.html

--- neuropixels.js
+++ neuropixels.js
@@ -7,7 +7,7 @@ export function createScatterPlot(chartElement) {
         .then(dataResponse => {
             data = dataResponse; // Assign data from the fetch response to the higher scoped variable
 
-            const width = 1000;
+            const width = 400;
             const height = 600;
             const imageWidth = 600; // Fixed width for the image
             const imageHeight = 400; // Fixed height for the image
@@ -17,7 +17,7 @@ export function createScatterPlot(chartElement) {
             const xScale = d3
                 .scaleLinear()
                 .domain([-d3.max(data, (d) => d.x), d3.max(data, (d) => d.x)])
-                .range([0, width - imageWidth]);
+                .range([0, width]);
 
             const yScale = d3
                 .scaleLinear()
@@ -44,21 +44,12 @@ export function createScatterPlot(chartElement) {
                 updateImage(imageUrl);
             });
 
-            // Position the scatter plot and image side by side
-            const scatterPlotLeft = 100; // Left position for the scatter plot
-            const scatterPlotTop = 1000; // Top position for the scatter plot
-            const imageLeft = width - imageWidth + scatterPlotLeft; // Left position for the image
-            const imageTop = scatterPlotTop; // Top position for the image
-
             // Append the image container
             const imageSvg = d3
                 .select(chartElement)
                 .append('svg')
                 .attr('width', imageWidth)
                 .attr('height', imageHeight)
-                .style('position', 'absolute')
-                .style('left', `${imageLeft}px`)
-                .style('top', `${imageTop}px`)
                 .attr('class', 'image-container');
 
             const imageElement = imageSvg
